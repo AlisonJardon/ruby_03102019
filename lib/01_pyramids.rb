f28df319d0307e632c287f70fa8130ff979c30a7
AlisonJardon <alison.jardon@gmail.com>
2019-10-03 17:56:24 +0200

    full pyramide

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,28 @@
+def half_pyramid
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	print "> "
+nombre = Integer(gets) rescue false
+while nombre == false || nombre < 1 || nombre > 25
+puts "Choisis un nombre entre 1 et 25"
+print "> "
+nombre = Integer(gets) rescue false
+end
+return nombre
+end
+
+def full_pyramid(nombre)
+	puts "Voici la pyramide :"
+  nombre.times do |n|
+    print '-' * (nombre - n)
+    puts '#' * (2 * n + 1)
+sleep 0.5
+end
+end
+
+def perform
+nombre = half_pyramid
+full_pyramid(nombre)
+end
+
+
+perform
